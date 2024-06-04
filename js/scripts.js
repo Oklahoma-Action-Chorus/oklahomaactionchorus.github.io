868031633e20b006b8fc8254530d4dc88edc9498
kbramlett <75955180+kbramlett@users.noreply.github.com>
2024-06-04 16:44:33 -0700

    Update scripts.js
    adding code to collapse dropdown on click

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,8 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(document).ready(function () {
+  $(".nav-link").click(function() {
+    $("#navbarResponsive").collapse("hide");
+  });
+});
